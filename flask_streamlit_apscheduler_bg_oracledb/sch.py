0a9bb583377bb34829060e0733a3eb506ed4f850
yhcho007 <yhcho007@naver.com>
2025-05-13 17:28:45 +0900

    Update sch.py

--- flask_streamlit_apscheduler_bg_oracledb/sch.py
+++ flask_streamlit_apscheduler_bg_oracledb/sch.py
@@ -3,9 +3,11 @@ import pandas as pd
 import subprocess
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
+import os
+import signal
 
 def fetch_tasks(connection):
-    query = "SELECT taskid, taskname, subprocee_starttime, task_status FROM SCH WHERE task_status != 'E'"
+    query = "SELECT taskid, taskname, subprocee_starttime, task_status FROM SCH WHERE task_status != 'S'"
     return pd.read_sql(query, connection)
 
 def update_task_status(connection, taskid, status):
@@ -20,7 +22,11 @@ def update_task_status(connection, taskid, status):
 
 def run_app_py(taskid):
     """Run the app.py script with taskid as an argument."""
-    subprocess.Popen(["python3", "app.py", taskid])  # taskid를 인자로 전달
+    process = subprocess.Popen(["python3", "app.py", taskid],
+                            start_new_session=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)  # taskid를 인자로 전달
+    return process
 
 def check_and_run_tasks():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -30,7 +36,7 @@ def check_and_run_tasks():
         
         for _, row in tasks.iterrows():
             if row['subprocee_starttime'].strftime("%Y-%m-%d %H:%M:%S") >= current_time:
-                run_app_py(row['taskid'])  # taskid를 인자로 넘김
+                process = run_app_py(row['taskid'])  # taskid를 인자로 넘김
                 update_task_status(connection, row['taskid'], 'I')
 
 def main():
